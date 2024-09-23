fa2f485e5340d12a1d0cdb9ccdbd53c9533c690d
mrovengerdev <rovenger.max@gmail.com>
2024-09-22 18:58:22 -0700

    feat(Assignment-1): added assignment 1 files

---
+++ Assignment1/parentheses.py
@@ -0,0 +1,50 @@
+import sys
+
+if len(sys.argv) > 1:
+    equation = sys.argv[1]  # Get the first argument
+else:
+    print("No argument passed")
+
+class Parentheses():
+    # Added stack functions
+    def push(self, stack, item):
+        stack.append(item)
+    
+    def pop(self, stack):
+        if not self.is_empty(stack):
+            return stack.pop()
+    
+    def is_empty(self, stack):
+        return len(stack) == 0
+    
+    # Parses through equation string and checks if for each parentheses open symbol '(' there is a closing symbol ')'.
+    def equationScanner(self, equation):
+        stack = []
+        for i in range(0, len(equation)):
+            if equation[i] == '(':
+                self.push(stack, equation[i])
+            elif equation[i] == ')':
+                if self.is_empty(stack):
+                    return False
+                self.pop(stack)
+        return self.is_empty(stack)
+    
+    def check(self, equation):
+        result = self.equationScanner(equation)
+        if result == True:
+            print("yes")
+        else:
+            print("no")
+
+if __name__ == "__main__":
+    Parentheses().check(equation)
+
+# Example run:
+# python parentheses.py "(()()(()))"
+# Example output:
+# yes
+
+# Example run:
+# python parentheses.py "(()()(())"
+# Example output:
+# no
---
+++ Assignment1/specs.md
@@ -0,0 +1,23 @@
+# Author
+Maxwell Rovenger - mrovenger@chapman.edu - 002398501
+
+# Methods
+calculator.py
+    - number():
+    - add():
+    - mul():
+    - expo():
+    - int():
+    - main():
+parentheses.py
+    - push():
+    - pop():
+    - is_empty():
+    - equationScanner(self, equation):
+    - check(self, equation):
+
+# Purpose
+
+# Internal Logic of Methods
+
+# General Logic of Program Flow
